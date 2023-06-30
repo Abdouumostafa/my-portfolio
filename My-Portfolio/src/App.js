37a8a1dc1479f2fcd6d9bdcaf1a6e4f5883052d4
Abdelrahman Mustafa <112385425+Abdouumostafa@users.noreply.github.com>
2023-06-30 15:01:08 +0300

    Add files via upload

---
+++ My-Portfolio/src/App.js
@@ -0,0 +1,23 @@
+import Navbar from "./sections/Navbar";
+import Homeland from "./sections/Homeland";
+import About from "./sections/About";
+import Skills from "./sections/Skills";
+import Projects from "./sections/Projects";
+import Contact from "./sections/Contact";
+import Footer from "./sections/Footer";
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <Homeland />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
---
+++ My-Portfolio/src/sections/About.js
@@ -0,0 +1,21 @@
+const About = () => {
+  return (
+    <section className="about-section" id="/about">
+      <h2 className="title">about me</h2>
+      <div className="underline"></div>
+      <div className="subsection">
+        <p>
+          I'm A React JS Developer <br /> Seeking a challenging opportunity as a
+          Developer in a well reputed firm, using latest technologies and skills
+          so as to be qualified for a leading position, where I can further
+          upgrade and develop my experience and knowledge in a mutually
+          beneficial relationship.
+        </p>
+        <button type="button" className="btn">
+          Resume
+        </button>
+      </div>
+    </section>
+  );
+};
+export default About;
