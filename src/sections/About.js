bdb8bf6d40d25101eaa598e27cb639c8a53fc89c
Abdelrahman Mostafa <112385425+Abdouumostafa@users.noreply.github.com>
2023-07-22 07:21:25 +0300

    Update About.js

--- src/sections/About.js
+++ src/sections/About.js
@@ -18,14 +18,11 @@ const About = () => {
       <div className="underline"></div>
       <div className="subsection">
         <p>
-          I'm A
-          <span className="text-animation">
-             React JS Developer <br /> Seeking a challenging opportunity as a
+          I'm A React JS Developer <br /> Seeking a challenging opportunity as a
             Developer in a well reputed firm, using latest technologies and
             skills so as to be qualified for a leading position, where I can
             further upgrade and develop my experience and knowledge in a
             mutually beneficial relationship.
-          </span>
         </p>
         <button type="button" class="btn2">
           <a href="https://drive.google.com/file/d/1W5GI1XBq_Jd5Ju5y7zMkSGQglTDTBxdf/view?usp=drive_link">
